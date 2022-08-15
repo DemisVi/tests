5c544b12d5c7397f41fc7e70865873e870d70464
Denis <87558735+DemisVi@users.noreply.github.com>
2022-08-15 12:00:22 +0300

    Interviev task 'equal objects constructors'

--- Program.cs
+++ Program.cs
@@ -1,16 +1,4 @@
-﻿var arr = Array.Empty<HelloWorld>();
+﻿var a = new string(new char[0]);
+var b = new string(new char[0]);
 
-var ne = new HelloWorld() { Num = 2, Pum = 5 };
-var arr2 = arr.Append(ne);
-
-System.Console.WriteLine(arr2.Count());
-foreach (var i in arr2)
-    System.Console.WriteLine(i);
-
-public class HelloWorld
-{
-    public int Num { get; set; } = 0;
-    public int Pum { get; set; } = 1;
-
-    public override string ToString() => $"Num: {Num} Pum: {Pum}";
-}
+System.Console.WriteLine(a == b);
