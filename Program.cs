bce1120bab87e00084b743092c6d3f1d0b9d69d2
Denis <87558735+DemisVi@users.noreply.github.com>
2022-08-22 18:07:18 +0300

    Interview crap

--- Program.cs
+++ Program.cs
@@ -1,78 +1,17 @@
-﻿using System.Text.Json;
+﻿int MAX = int.Parse(args[0]);
 
-var str = ConfigHelper.Token;
-System.Console.WriteLine(str);
-// ConfigHelper.AddUser("123123123123");
-// ConfigHelper.Save();
-
-public static class ConfigHelper
+for (int i = 0; i < MAX; i++)
 {
-    private static Config config;
-    public static string Token => config.Token;
-    public static List<string> Users => config.Users;
-    static ConfigHelper()
+    for (int j = 0; j < MAX; j++)
     {
-        try
-        {
-            using var fileStream = File.OpenRead("config.json");
-            config = JsonSerializer.Deserialize<Config>(fileStream);
-        }
-        catch (Exception ex)
-        {
-            config = ex switch
-            {
-                JsonException => ReadAsLine(),
-                FileNotFoundException => CreateBlankConfig(),
-                _ => throw new Exception("Can't read or create config.json")
-            };
-        }
-
-        static Config ReadAsLine()
-        {
-            try
-            {
-                var line = File.ReadAllLines("./config.json")[0].Trim('"');
-
-                config = new Config(line);
-                Save();
-
-                return config;
-            }
-            catch (IndexOutOfRangeException)
-            {
-                return CreateBlankConfig();
-            }
-        }
-
-        static Config CreateBlankConfig()
-        {
-            config = new Config();
-            Save();
-            return config;
-        }
+        System.Console.Write(GetSymbol(i, j, MAX));
     }
+    System.Console.WriteLine();
+}
 
-    private static void Save(Config config)
-    {
-        using var fileStream = File.OpenWrite("config.json");
-        JsonSerializer.Serialize(fileStream, config);
-    }
-    public static void Save() => Save(config);
-
-    public static void AddUser(string id)
-    {
-        if (!Users.Contains(id))
-        {
-            Users.Add(id);
-        }
-    }
-
-    private struct Config
-    {
-        public string Token { get; set; } = string.Empty;
-        public List<string> Users { get; set; } = new();
-
-        public Config(string token) => Token = token;
-        public Config() => Token = "token";
-    }
+char GetSymbol(int i, int j, int mAX)
+{
+    return i == j || i == mAX - 1 - j ||
+           i == 0 || i == mAX - 1 ||
+           j == 0 || j == mAX - 1 ? '#' : ' ';
 }
