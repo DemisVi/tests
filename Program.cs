19c6e4012465306c8de7ce7da898c05e87f35e09
Denis <87558735+DemisVi@users.noreply.github.com>
2022-08-24 21:38:02 +0300

    Switch expression

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿var c = new[] { '1', '2', '3', '4', '5', '6' };
+var s = new string(c);
+
+var res = SW(s);
+System.Console.WriteLine(res);
+
+string SW(string ass) => ass switch
+{
+    string { Length: > 5 } => "a" 
+};
