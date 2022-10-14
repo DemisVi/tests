6e3d26adb17e7420081744f1bc9d312bba5018f8
DemisVi <87558735+DemisVi@users.noreply.github.com>
2022-10-14 18:59:51 +0300

    PATH parsing

--- Program.cs
+++ Program.cs
@@ -9,48 +9,6 @@ using System.IO;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-try
-{
-    new KataTests().BasicTests();
-}
-catch (System.Exception ex)
-{
-    System.Console.WriteLine(ex.Message);
-}
+var path = Environment.GetEnvironmentVariable("PATH")?.Split(';').Select(x => Path.Combine(x, "ls.exe")).Where(x => File.Exists(x)).FirstOrDefault();
 
-public class Kata
-{
-    public static int[] SortArray(int[] array)
-    {
-        for (var i = 0; i < array.Length; i++)
-        {
-            if (array[i] % 2 == 0) continue;
-
-            for (var j = i + 1; j < array.Length; j++)
-                if (array[j] % 2 != 0)
-                    if (array[i] > array[j])
-                        Swap(ref array[i], ref array[j]);
-        }
-
-        return array;
-
-        void Swap(ref int a, ref int b)
-        {
-            b ^= a;
-            a ^= b;
-            b ^= a;
-        }
-    }
-}
-
-[TestFixture]
-public class KataTests
-{
-    [Test]
-    public void BasicTests()
-    {
-        Assert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
-        Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
-        Assert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
-    }
-}
+System.Console.WriteLine(path);
