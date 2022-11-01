fffe3f667c8a42d16f4ee1c31bde894d361ed8a9
DemisVi <87558735+DemisVi@users.noreply.github.com>
2022-11-01 21:29:01 +0300

    Clear and add ftdi reference

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Security.Cryptography;
 using System.Linq;
-using System.Text;
-using System.Timers;
-using System.Threading;
-using System.Threading.Tasks;
-using System.IO;
-using System.Collections.Generic;
-using NUnit.Framework;
+using FTD2XX_NET;
 
-var path = Environment.GetEnvironmentVariable("PATH")?.Split(';').Select(x => Path.Combine(x, "ls.exe")).Where(x => File.Exists(x)).FirstOrDefault();
-
-System.Console.WriteLine(path);
+var ft = new FTDI();
