005270125b91369bcdbafeba8e4bfb9e815c7721
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-07 15:59:18 +0300

    delegate definition

--- Modem.cs
+++
@@ -1,6 +0,0 @@
-public class Modem
-{
-    public string AttachedTo { get; set; } = string.Empty;
-    public string Serial { get; set; } = string.Empty;
-
-}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.IO.Ports;
 using System.Text;
-using System.Management;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
-using ROOT.CIMV2.Win32;
 
-var devs = await new TelitLocator().LocateDevicesAsync();
+var d = delegate () { System.Console.WriteLine("qwe");};
 
-foreach (var item in devs)
-{
-    System.Console.WriteLine(item.AttachedTo + " " + item.Serial);
-}
+d();
