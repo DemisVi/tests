7d367a4b43d747b3b053f5cfd0203a00772e4212
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-07 15:36:50 +0300

    TelitLocator

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Management;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
-using FTD2XX_NET;
+using ROOT.CIMV2.Win32;
 
-var ft = new FTDI();
+var devs = await new TelitLocator().LocateDevicesAsync();
+
+foreach (var item in devs)
+{
+    System.Console.WriteLine(item.AttachedTo + " " + item.Serial);
+}
