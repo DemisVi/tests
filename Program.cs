6302599a39c2642a421d0074bce9058a01e9f0c7
DemisVi <87558735+DemisVi@users.noreply.github.com>
2022-11-22 22:44:33 +0300

    Management object searcher

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Linq;
+using System.Reflection;
+using System.Management;
 
-var d = delegate () { System.Console.WriteLine("qwe");};
+using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PNPEntity WHERE Manufacturer LIKE 'Samsung%'");
 
-d();
+var query = searcher.Get();
+
+var @enum = query.GetEnumerator();
+@enum.MoveNext();
+
+foreach (var i in @enum.Current.Properties)
+{
+    Type t = i.GetType();
+    Console.WriteLine("Type is: {0}", t.Name);
+    var props = t.GetProperties();
+    Console.WriteLine("Properties (N = {0}):",
+                      props.Length);
+    foreach (var prop in props)
+        if (prop.GetIndexParameters().Length == 0)
+            Console.WriteLine("   {0} ({1}): {2}", prop.Name,
+                              prop.PropertyType.Name,
+                              prop.GetValue(i));
+        else
+            Console.WriteLine("   {0} ({1}): <Indexed>", prop.Name,
+                              prop.PropertyType.Name);
+}
