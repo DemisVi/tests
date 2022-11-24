0ead64f22591f95e90396bbf4822c8d9e907b5c2
d.liudkoi <114064097+DemisVi-work@users.noreply.github.com>
2022-11-24 09:22:09 +0300

    Merge fix

--- Program.cs
+++ Program.cs
@@ -1,33 +1,5 @@
 ﻿using System;
 using System.Linq;
-<<<<<<< HEAD
-using System.Reflection;
-using System.Management;
-
-using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PNPEntity WHERE Manufacturer LIKE 'Samsung%'");
-
-var query = searcher.Get();
-
-var @enum = query.GetEnumerator();
-@enum.MoveNext();
-
-foreach (var i in @enum.Current.Properties)
-{
-    Type t = i.GetType();
-    Console.WriteLine("Type is: {0}", t.Name);
-    var props = t.GetProperties();
-    Console.WriteLine("Properties (N = {0}):",
-                      props.Length);
-    foreach (var prop in props)
-        if (prop.GetIndexParameters().Length == 0)
-            Console.WriteLine("   {0} ({1}): {2}", prop.Name,
-                              prop.PropertyType.Name,
-                              prop.GetValue(i));
-        else
-            Console.WriteLine("   {0} ({1}): <Indexed>", prop.Name,
-                              prop.PropertyType.Name);
-}
-=======
 using System.Management;
 using System.Diagnostics;
 using System.IO.Ports;
@@ -80,4 +52,3 @@ void ResolveEvent(object? obj, EventArrivedEventArgs e)
         System.Console.WriteLine(queryObj["AttachedTo"]);
     }
 }
->>>>>>> 5fe246c (WQL queries)
