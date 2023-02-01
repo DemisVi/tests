716b65a2915c2a0792f17d09d764a5b63b0195eb
DemisVi <87558735+DemisVi@users.noreply.github.com>
2023-02-01 14:39:43 +0300

    updater

--- Program.cs
+++ Program.cs
@@ -8,37 +8,40 @@ using System.Management;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using FTD2XX_NET;
-using Wrench.Model;
 
-var cu1 = new ContactUnit("CANCOM", 200D, System.Console.WriteLine);
+var sourcePath = Path.Combine(Environment.CurrentDirectory, "./Data/");
+var destPath = "C:/Wrench/Data/";
+var factoryPath = Directory.GetFiles(sourcePath, "*.cfg", SearchOption.AllDirectories).First();
+var factoryDir = Path.GetDirectoryName(factoryPath);
+var factoryName = Path.GetFileName(factoryPath);
 
-Task.Factory.StartNew(() => Test(cu1));
+UpdateCFG(factoryDir);
 
-Thread.Sleep(Timeout.Infinite);
+CopyPackage(sourcePath, destPath);
 
-void Test(ContactUnit cu)
+
+
+void CopyPackage(string? source, string? dest)
 {
-    int up = 0, down = 0, fail = 0, jamm = 0;
-    while (true)
-    {
-        cu.SetOuts(Outs.Pn1 | Outs.Cyan);
-        // Console.ReadKey();
-        if (cu.WaitForBits(Sensors.Pn1_Up, 5) != Sensors.None)
-        {
-            up++;
-        }
-        else fail++;
-        cu.SetOuts(Outs.None | Outs.Magenta);
-        if (cu.WaitForBits(Sensors.Pn1_Down, 5) != Sensors.None)
-        {
-            down++;
-        }
-        else jamm++;
+    if (string.IsNullOrEmpty(source)) throw new FileNotFoundException("could not copy from nowhere");
+    if (string.IsNullOrEmpty(dest)) throw new FileNotFoundException("could not copy to nowhere");
+    var dirs = Directory.GetDirectories(source, "*", SearchOption.AllDirectories);
+    var files = Directory.GetFiles(source, "*", SearchOption.AllDirectories);
 
-        // lock (_lock)
-        {
-            System.Console.WriteLine("{0}> up: {1} / down: {4} / fail: {2} / jamm: {3}", cu.PortName, up, fail, jamm, down);
-        }
-    }
+    foreach (var dir in dirs) Directory.CreateDirectory(dir.Replace(source, dest));
+    foreach (var fi in files) File.Copy(fi, fi.Replace(source, dest));
+}
+
+void UpdateCFG(string? path)
+{
+    if (string.IsNullOrEmpty(path)) throw new FileNotFoundException("factory.cfg path not found");
+
+    var fac = new FactoryCFG(path);
+    fac.ReadFactory();
+
+    fac.SerialNumber += (Base34)"1000";
+
+    System.Console.WriteLine($"Serial: {fac.SerialNumber}");
+
+    fac.SaveFactory();
 }
