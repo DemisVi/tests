56eddec4b46cbb9c442885efa0b3de10dbcc52a0
d.liudskoi <114064097+DemisVi-work@users.noreply.github.com>
2023-05-31 12:52:54 +0300

    variable set

--- Program.cs
+++ Program.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.IO;
-using System.IO.Ports;
-using System.Diagnostics;
-using System.Text;
-using System.Linq;
-using System.Management;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
-System.Console.WriteLine(BitConverter.ToString(new byte[] {0x01, 0x02, 0xAA}));
+var v = Environment.GetEnvironmentVariable("path");
 
-public static class DayOfWeekExtension
-{
-    public static bool IsWeekend(this DayOfWeek day) => (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday);
-}
+Environment.SetEnvironmentVariable("path", "C:\\;" + v, EnvironmentVariableTarget.Machine);
+
+System.Console.WriteLine(Environment.GetEnvironmentVariable("path"));
