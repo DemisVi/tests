6b7caff219abfc6a0691279bbd4e159f2c8b5ecc
d.liudskoi <114064097+DemisVi-work@users.noreply.github.com>
2023-10-30 19:30:15 +0300

    refactor FtSerialPort

--- FtSerialPort/FtSerialPort.cs
+++ FtSerialPort/FtSerialPort.cs
@@ -3,27 +3,10 @@ using System.Linq;
 using System.IO.Ports;
 using System.Management;
 
-#pragma warning disable CA1416
-
 public class FtSerialPort : SerialPort
 {
     public new int BaudRate { get; } = 115200;
     public FtSerialPort(string portName) : base(portName) => base.BaudRate = BaudRate;
 
-    public new static string[] GetPortNames()
-    {
-        using var searcher = new ManagementObjectSearcher(FtdiQueries.FtdiSerialDevice);
-        var objs = searcher.Get();
-
-        if (objs is { Count: > 0 })
-            return objs.Cast<ManagementObject>().Select(x => GetSerialName(x)).ToArray();
-        else
-            return Array.Empty<string>();
-    }
-
-    private static string GetSerialName(ManagementObject x) => x.GetText(TextFormat.Mof).Split(
-        new char[] { '(', ')' },
-        StringSplitOptions.RemoveEmptyEntries).First(x => x.StartsWith("COM"));
+    public new static string[] GetPortNames() => SerialPortSearcher.GetPortNames(WqlQueries.ObjectFtdiSerial);
 }
-
-#pragma warning restore CA1416
--- FtSerialPort/FtdiQueries.cs
+++ FtSerialPort/FtdiQueries.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Management;
 
-#pragma warning disable CA1416
 
-public class FtdiQueries
+public static class FtdiQueries
 {
+#pragma warning disable CA1416
 
     public static WqlObjectQuery FtdiSerialDevice { get; } = new(
         "SELECT Caption FROM Win32_PnPEntity WHERE Manufacturer = 'FTDI' AND Caption LIKE 'USB Serial Port (%)'");
 
-}
-
 #pragma warning restore CA1416
+}
